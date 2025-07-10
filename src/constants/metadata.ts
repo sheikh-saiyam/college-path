5cdd345ea5e15997b546497b3cad73b8c2ce22aa
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-10 16:18:10 +0600

    Added siteMetadata in constants folder

---
+++ src/constants/metadata.ts
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+
+export const siteMetadata: Metadata = {
+  title: "College Path | Book Your Dream College",
+  description:
+    "College Path is a user-friendly platform to explore top colleges, compare facilities, book admissions, and read real student reviews all in one place.",
+  keywords: [
+    "college booking",
+    "college admission",
+    "college details",
+    "college review platform",
+    "College Path",
+    "admission portal",
+    "compare colleges",
+    "book college online",
+    "college rating",
+    "research paper portal",
+  ],
+  authors: [{ name: "Sheikh Saiyam", url: "https://college-path.vercel.app" }],
+  creator: "Sheikh Saiyam",
+  openGraph: {
+    title: "College Path | Book Your Dream College",
+    description:
+      "Explore and book colleges with full details on admission, events, research, and sports. Read reviews from real students only on College Path.",
+    url: "https://college-path.vercel.app",
+    siteName: "College Path",
+    images: [
+      {
+        url: "/banner.jpg",
+        width: 1200,
+        height: 630,
+        alt: "College Path - Book your dream college",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+  },
+  metadataBase: new URL("https://college-path.vercel.app"),
+  // themeColor: "#3E4EB8",
+  icons: { icon: "/favicon.ico" },
+};
