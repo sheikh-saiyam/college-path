0d44caec008dd5e7dd794587eb0ca41e76b2ffbe
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-11 22:42:40 +0600

    dbConnect function added

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,8 @@
+import clientPromise from "./mongodb";
+
+export const dbConnect = async (collectionName: string) => {
+  const client = await clientPromise;
+  return client.db("college_path_DB").collection(collectionName);
+};
+
+export const collections = { admissions: "admissions" };
