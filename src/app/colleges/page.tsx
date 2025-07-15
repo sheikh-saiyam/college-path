d46a3b86c8aeb5dcf4bf57c0c97af753eaff83c8
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-15 19:48:29 +0600

    Added Metadata on colleges page

--- src/app/colleges/page.tsx
+++ src/app/colleges/page.tsx
@@ -1,4 +1,21 @@
 import FeaturedColleges from "@/components/home/FeaturedColleges";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "College Path | Explore All Colleges",
+  description:
+    "Browse a wide selection of top colleges, compare features, facilities, and admission timelines to find the best fit for your future.",
+  keywords: [
+    "explore colleges",
+    "college list",
+    "top colleges",
+    "compare colleges",
+    "College Path",
+    "college admission guide",
+    "best colleges in Bangladesh",
+    "college details platform",
+  ],
+};
 
 const CollegesPage = () => {
   return <FeaturedColleges showAll={true} />;
